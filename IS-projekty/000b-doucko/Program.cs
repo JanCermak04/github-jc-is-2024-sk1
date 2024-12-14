cb06500d2cbd2e118d6151f518469ed91d018c31
Jan Cermak <Jan.Cermak2@tul.cz>
2024-12-14 21:13:23 +0100

    #doucko

--- IS-projekty/000b-doucko/Program.cs
+++ IS-projekty/000b-doucko/Program.cs
@@ -32,6 +32,34 @@ class Program
         Console.WriteLine("--------------------------------------------------");
         Console.WriteLine();
 
+
+                // Min and Max
+        int min = MY[0], max = MY[0];
+        for (int i = 0; i < pocet_prvku; i++)
+        {
+            if (min > MY[i]) min = MY[i];
+            if (max < MY[i]) max = MY[i];
+        }
+
+        Console.WriteLine("Nejmensi cislo je " + min + " na pozicich:");
+        for (int i = 0; i < pocet_prvku; i++)
+        {
+            if (MY[i] == min)
+                Console.Write((i + 1) + " ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Nejvetsi cislo je " + max + " na pozicich:");
+        for (int i = 0; i < pocet_prvku; i++)
+        {
+            if (MY[i] == max)
+                Console.Write((i + 1) + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine();
+
+
         Stopwatch stopwatch = new Stopwatch();
 
         // CombSort
@@ -142,39 +170,11 @@ class Program
 
 
 
-        // Min and Max
-        int min = MY[0], max = MY[0];
-        for (int i = 0; i < pocet_prvku; i++)
-        {
-            if (min > MY[i]) min = MY[i];
-            if (max < MY[i]) max = MY[i];
-        }
-
-        Console.WriteLine("Nejmensi cislo je " + min + " na pozicich:");
-        for (int i = 0; i < pocet_prvku; i++)
-        {
-            if (MY[i] == min)
-                Console.Write((i + 1) + " ");
-        }
-        Console.WriteLine();
-
-        Console.WriteLine("Nejvetsi cislo je " + max + " na pozicich:");
-        for (int i = 0; i < pocet_prvku; i++)
-        {
-            if (MY[i] == max)
-                Console.Write((i + 1) + " ");
-        }
-        Console.WriteLine();
-
-
-
-
-        Console.WriteLine("--------------------------------------------------");
-        Console.WriteLine();
        
        
 
 int fourthLargest = -1; 
+int sixthlargest = - 1;
 
 // Použijeme LINQ k odstranění duplicit
 var uniqueNumbers = MY.Distinct().ToArray(); // Odstraníme duplicity
@@ -185,6 +185,16 @@ if (uniqueNumbers.Length >= 4)
 {
     fourthLargest = uniqueNumbers[3]; // 4. největší číslo
     Console.WriteLine("Ctvrte nejvetsi cislo je: " + fourthLargest);
+}
+else
+{
+    Console.WriteLine("Pole nemá dostatečný počet unikátních prvků.");
+}
+
+if (uniqueNumbers.Length >= 6)
+{
+    sixthlargest = uniqueNumbers[5]; // 4. největší číslo
+    Console.WriteLine("Šesté nejvetsi cislo je: " + sixthlargest);
 }
 else
 {
@@ -274,8 +284,55 @@ Console.WriteLine();
             }
         }
     
-        Console.WriteLine("Kosoctverec:");
-      int height = 5; // Výška obdélníku
+
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine();
+
+        // Define the initial number of elements in the sequence
+int pocet_rady = 9;
+
+// Function to generate an arithmetic sequence and calculate its sum
+void GenerateArithmeticSequence(int start, int diff, int count)
+{
+    if (count <= 0)
+    {
+        Console.WriteLine("Počet prvků musí být kladné číslo.");
+        return;
+    }
+
+    Console.WriteLine("Aritmetická posloupnost:");
+    int sum = 0;
+    for (int i = 0; i < count; i++)
+    {
+        int current = start + i * diff; // Arithmetic sequence formula
+        Console.Write(current + " ");
+        sum += current; // Add the current element to the sum
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Součet aritmetické posloupnosti: " + sum);
+}
+
+// Ensure fourthLargest and sixthLargest are valid
+if (uniqueNumbers.Length >= 6)
+{
+    
+    int sixthLargest = uniqueNumbers[5];  // 6th largest unique number
+
+    // Call the function with appropriate values
+    GenerateArithmeticSequence(fourthLargest, sixthLargest, pocet_rady);
+}
+else
+{
+    Console.WriteLine("Pole nemá dostatečný počet unikátních prvků pro výpočet aritmetické posloupnosti.");
+}
+
+
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine();
+
+       Console.WriteLine("Kosoctverec:");
+        int height = 5; // Výška obdélníku
         int width = 10; // Šířka obdélníku
 
         for (int i = 0; i < height; i++)
@@ -283,16 +340,23 @@ Console.WriteLine();
             // Přidání odsazení pro kosý efekt
             Console.Write(new string(' ', i));
 
-            // Vykreslení řádku obdélníku
-            Console.WriteLine(new string('*', width));
+            if (i == 0 || i == height - 1)
+            {
+                // Horní a dolní vodorovná strana (2 řádky)
+                Console.WriteLine(new string('*', width));
+                Console.Write(new string(' ', i)); // Přidání odsazení pro druhý řádek
+                Console.WriteLine(new string('*', width));
+            }
+            else
+            {
+                // Boční svislé strany (prázdný vnitřek)
+                Console.WriteLine("*" + new string(' ', width - 2) + "*");
+            }
         }
 
 
-
-
-
     Console.WriteLine("šipka");
-        int vyska = 5; // Výška šipky
+        int vyska = wholePart; // Výška šipky
 
         // Horní část šipky (trojúhelník)
         for (int i = 0; i < vyska; i++)
@@ -314,7 +378,37 @@ Console.WriteLine();
             Console.WriteLine("***"); // Jedna hvězdička
         }
 
+        
+        
+        
+         Console.WriteLine("Šipka dolů");
 
+
+        int vyskaSipky = wholePart; // Výška trojúhelníkové části šipky
+        int vyskaTelo = wholePart;  // Výška těla šipky
+
+        // Tělo šipky (svislý pruh s prázdným vnitřkem)
+        for (int i = 0; i < vyskaTelo; i++)
+        {
+            Console.Write(new string(' ', vyskaSipky - 2)); // Odsazení
+            Console.WriteLine("* *"); // Šířka těla s prázdným vnitřkem
+        }
+
+        // Dolní část šipky (trojúhelník směřující dolů)
+        for (int i = vyskaSipky - 1; i >= 0; i--)
+        {
+            Console.Write(new string(' ', vyskaSipky - i - 1)); // Odsazení
+            if (i == 0)
+            {
+                Console.WriteLine("*"); // Spodní špička
+            }
+            else
+            {
+                Console.Write("*"); // Levý okraj
+                Console.Write(new string(' ', 2 * i - 1)); // Vnitřní mezera
+                Console.WriteLine("*"); // Pravý okraj
+            }
+        }
 
 
 
@@ -370,10 +464,48 @@ for (int i = hight - 1; i >= 0; i--)
     }
 }
 
+// ctverec s uhloprickou //
+
+
+        int size = 10; // Výška čtverce
+
+        // Horní hrana
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+
+        // Vnitřek čtverce
+        for (int i = 1; i < size - 1; i++)
+        {
+            Console.Write("*"); // Levý okraj čtverce
+
+            for (int j = 1; j < size - 1; j++)
+            {
+                if (j == i) // Podmínka pro uhlopříčku hvězdiček
+                    Console.Write("*");
+                else if (j > i) // Pravá strana od uhlopříčky
+                    Console.Write("0");
+                else
+                    Console.Write(" "); // Mezera vlevo od uhlopříčky
+            }
+
+            Console.Write("*"); // Pravý okraj čtverce
+            Console.WriteLine();
+        }
+
+        // Spodní hrana
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+
+
             // Zajištění, že konzolové okno zůstane otevřené
             Console.WriteLine("Stiskněte libovolnou klávesu pro ukončení programu...");
             Console.ReadKey();
         }
     }
-
 
